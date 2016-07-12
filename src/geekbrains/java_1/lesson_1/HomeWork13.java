7fb0e34d8380fba0141c6c3c097891ecfb55531b
DragSer <dragser@gmail.com>
2016-07-12 12:26:10 +0300

    the job is done

--- src/geekbrains/java_1/lesson_1/HomeWork13.java
+++ src/geekbrains/java_1/lesson_1/HomeWork13.java
@@ -1,7 +1,21 @@
 package geekbrains.java_1.lesson_1;
 
 /**
- * Created by xxx on 12.07.2016.
+ * Домашнее задание по курсу: * Java 1 - Урок 1 - Задание 3
+ * Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
+ *  где a,b,c,d – входные параметры этого метода.
+ * Created by xxx on 09.07.2016.
  */
 public class HomeWork13 {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 2;
+        int c = 4;
+        int d = 2;
+
+        System.out.println("a * (b + (c / d)) = " + calc(a, b, c, d));
+    }
+    private static int calc (int a, int b, int c, int d){
+        return a * (b + (c / d));
+    }
 }
