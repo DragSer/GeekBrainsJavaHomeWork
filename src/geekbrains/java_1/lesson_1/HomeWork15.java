3d2cfb6f8e597b871a05f1422522b6e1f512dc08
DragSer <dragser@gmail.com>
2016-07-12 12:26:19 +0300

    the job is done

--- src/geekbrains/java_1/lesson_1/HomeWork15.java
+++ src/geekbrains/java_1/lesson_1/HomeWork15.java
@@ -1,7 +1,25 @@
 package geekbrains.java_1.lesson_1;
 
 /**
- * Created by xxx on 12.07.2016.
+ * Домашнее задание по курсу: * Java 1 - Урок 1 - Задание 5
+ * Написать метод, который определяет является ли год високосным.
+ * Каждый 4й год является високосным, кроме каждого 100го, при этом каждый 400й – високосный.
+ * Created by xxx on 09.07.2016.
  */
 public class HomeWork15 {
+    public static void main(String[] args) {
+        int year = 2016;
+
+        if(vyear(year) == true) {
+            System.out.println("Год " + year + " високосный.");
+        } else {
+            System.out.println("Год " + year + " не високосный.");
+        }
+    }
+
+    private static boolean vyear(int y){
+        boolean y2 = false;
+        if(y%4==0 && y%100 !=0 || y%400==0) { y2 = true; }
+        return y2;
+    }
 }
