e4bc11f0db8b096721440eb691f150afd6531993
DragSer <dragser@gmail.com>
2016-07-12 12:26:00 +0300

    the job is done

--- src/geekbrains/java_1/lesson_1/HomeWork11.java
+++ src/geekbrains/java_1/lesson_1/HomeWork11.java
@@ -1,7 +1,12 @@
 package geekbrains.java_1.lesson_1;
 
 /**
- * Created by xxx on 12.07.2016.
+ * Домашнее задание по курсу: * Java 1 - Урок 1 - Задание 1
+ * Создать пустой проект в IntelliJ IDEA и прописать метод main()
+ * Created by xxx on 09.07.2016.
  */
 public class HomeWork11 {
+    public static void main(String[] args){
+
+    }
 }
