784529111a476b375dc69f74482b3961cd1f4f93
DragSer <dragser@gmail.com>
2016-07-17 16:30:41 +0300

    111

--- src/geekbrains/tmp/TestTmp.java
+++ src/geekbrains/tmp/TestTmp.java
@@ -4,4 +4,15 @@ package geekbrains.tmp;
  * Created by xxx on 12.07.2016.
  */
 public class TestTmp {
+    public static void main(String[] args) {
+        //int a = 2;
+        //System.out.print(a);
+        proc2();
+    }
+
+    private static void proc2() {
+        int a = 2;
+        System.out.print(a);
+    }
+
 }
