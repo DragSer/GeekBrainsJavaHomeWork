b8164d1c825681a4b7fa3c8a268d020f24fa2a4e
DragSer <dragser@gmail.com>
2016-07-22 11:28:11 +0300

    Home Work 4

--- src/geekbrains/java_1/lesson_4/Employee.java
+++ src/geekbrains/java_1/lesson_4/Employee.java
@@ -1,7 +1,36 @@
 package geekbrains.java_1.lesson_4;
-
 /**
- * Created by xxx on 20.07.2016.
+ * 1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
+ * 2. Конструктор класса должен заполнять эти поля при создании объекта;
+ * 3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
+ * 4. Создать массив из 5 сотрудников. С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+ *  * Created by xxx on 20.07.2016.
  */
 public class Employee {
+    private String name;    // ФИО сотрудника
+    private String position;// Должность
+    private String email;   // eMail
+    private String phone;   // Телефон
+    private int salary;     // Зарплата
+    private int age;        // Возраст
+    private static int employee_count;
+
+    public Employee(String name, String position, String email, String phone, int salary, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+        employee_count++;
+    }
+
+    public static int getEmployee_count(){return employee_count; }
+
+    public void printInfo(){
+        System.out.println(name+"\t"+position+"\t"+email+"\t"+phone+"\t"+salary+"\t"+age);
+    }
+
+    public int getAge(){ return age; }
+
 }
--- src/geekbrains/java_1/lesson_4/MainClass.java
+++ src/geekbrains/java_1/lesson_4/MainClass.java
@@ -1,7 +1,27 @@
 package geekbrains.java_1.lesson_4;
 
-/**
- * Created by xxx on 22.07.2016.
- */
-public class MainClass {
+public class MainClass extends Employee{
+    public MainClass(String name, String position, String email, String phone, int salary, int age) {
+        super(name, position, email, phone, salary, age);
+    }
+
+    public static void main(String[] args) {
+        Employee[] empl = new Employee[5];
+        empl[0] = new Employee("Иванов", "Насяльник", "bos@mail.ru", "100-00-01", 100000, 50);
+        empl[1] = new Employee("Петров", "Бухгалтер", "pet@mail.ru", "100-00-02", 90000, 25);
+        empl[2] = new Employee("Сидоров", "Слесарь 1", "sid@mail.ru", "100-00-03", 80000, 41);
+        empl[3] = new Employee("Бородач", "Охранник", "bor@mail.ru", "100-00-04", 30000, 35);
+        empl[4] = new Employee("Джамшут", "Грузчик 1", "gru@mail.ru", "100-00-05", 30000, 43);
+
+        int employee_count = Employee.getEmployee_count();
+        System.out.println("Общее Количество сотрудников = " + employee_count);
+        System.out.println("Из них сотрудники старше 40 лет:");
+        for (int i = 0; i < empl.length; i++) {
+            if (empl[i].getAge() >= 40) empl[i].printInfo();
+        }
+    }
+
+
+
+
 }
