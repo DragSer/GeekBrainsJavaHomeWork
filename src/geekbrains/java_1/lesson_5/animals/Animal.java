6d8c513ca6c1f09b44b853e32b763942bad8fa5e
DragSer <dragser@gmail.com>
2016-07-25 18:03:13 +0300

    HomeWork5 v1.0

--- src/geekbrains/java_1/lesson_5/animals/Animal.java
+++ src/geekbrains/java_1/lesson_5/animals/Animal.java
@@ -1,7 +1,18 @@
 package geekbrains.java_1.lesson_5.animals;
 
-/**
- * Created by xxx on 22.07.2016.
- */
-public class Animal {
+public abstract class Animal {
+
+    protected String name;
+
+    public Animal(String name){ this.name = name; }
+
+    public void moveOnGround(){ System.out.print(name + " бегает"); }
+    public void moveOnGround(int speed){ System.out.print(name + " бегает"); }
+
+    public void swim(){System.out.print(name);}
+    public void swim(String swim){System.out.print(name);}
+
+    public void jump(){ System.out.print(name + " прыгает"); }
+    public void jump(int jump){ System.out.print(name + " прыгает"); }
+
 }
