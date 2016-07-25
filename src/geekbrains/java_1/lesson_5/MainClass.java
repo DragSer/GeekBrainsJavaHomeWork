1297bc520dd1a102d572981c1335d6957730f476
DragSer <dragser@gmail.com>
2016-07-25 18:04:03 +0300

    HomeWork5 v1.0

--- src/geekbrains/java_1/lesson_5/MainClass.java
+++ src/geekbrains/java_1/lesson_5/MainClass.java
@@ -1,7 +1,94 @@
 package geekbrains.java_1.lesson_5;
-
 /**
- * Created by xxx on 22.07.2016.
+ * 1. Создать несколько классов:
+ *    Собака, Лошадь, Кот - с наследованием от родительского класса Животное.
+ * 2. Все классы должны иметь возможность выполнять действия:
+ *    бежать, плыть, перепрыгивать препятствие.
+ * 3. У каждого класса свои ограничения на каждое из действий
+ *    (например, высота прыжка, собака может перепрыгнуть максимум полуметровое препятствие,
+ *    кот 1-2 метровое; считаем, что коты не умеют плавать и т.д.).
+ *    Каждое действие должно выводить результат в консоль
+ *    (смогло животное выполнить действие или нет).
+ * Created by dragser on 22.07.2016.
  */
+import geekbrains.java_1.lesson_5.animals.Animal;
+import geekbrains.java_1.lesson_5.animals.Cat;
+import geekbrains.java_1.lesson_5.animals.Dog;
+import geekbrains.java_1.lesson_5.animals.Horse;
+
+import java.util.Scanner;
+
+
 public class MainClass {
+
+    private static final int ANIMALS_COUNT = 3;
+    private int speed;
+    private String swim;
+    private int jump;
+
+
+    public static void main(String[] args) {
+        Scanner input;
+        input = new Scanner(System.in);
+        Animal[] animals = new Animal[ANIMALS_COUNT];
+        animals[0] = new Cat("Кошка");
+        animals[1] = new Dog("Собака");
+        animals[2] = new Horse("Лошадь");
+
+        int speed;      //Скорость бега
+        String swim;    // Плавает ли животное
+        int jump;       // Высота прыжка
+
+
+        System.out.println("ЗооТест");
+
+        System.out.println("С какой максимальной скоростью (км/ч) может бежать лошадь?");
+        System.out.print(">> ");
+        speed = input.nextInt();
+
+//        while (true) {
+//            speed = input.nextInt();
+//            if (speed < 0) {
+//                System.out.println("Скорость должна быть не отрицательным числом. Повторите ввод:");
+//            }else{
+//                break;
+//            }
+//        }
+
+        System.out.println("Плавает ли " + animals[0]+"? (Y/N)");
+        while (true) {
+            swim = input.next();
+            swim = swim.toUpperCase();
+            if (swim.equals("Y") || swim.equals("N")) {
+                break;
+            }else{
+                System.out.println("Введите Y или N");
+            }
+        }
+
+        System.out.println("На какую высоту (м) прыгает собака?");
+        jump = input.nextInt();
+
+//        while(true) {
+////            jump = input.nextInt();
+//            jump = 0;
+//            if(input.hasNextInt() && jump >= 0) {
+//                System.out.println("Ответы приняты." + jump);
+//                break;
+//            } else {
+//                System.out.println("Высота должна быть не отрицательным числом. Повторите ввод:");
+//
+//            }
+//        }
+
+        System.out.println("-------------------------------------");
+        System.out.println("Правильные ответы:");
+        animals[2].moveOnGround(speed);
+        System.out.println();
+        animals[0].swim(swim);
+        System.out.println();
+        animals[1].jump(jump);
+
+        input.close();
+    }
 }
