bafce46cf6ae9995e3c35dc81dfb083e7aadcf64
DragSer <dragser@gmail.com>
2016-07-25 18:04:24 +0300

    test

--- src/geekbrains/tmp/MyLib.java
+++ src/geekbrains/tmp/MyLib.java
@@ -1,7 +1,21 @@
 package geekbrains.tmp;
-
 /**
- * Created by xxx on 24.07.2016.
+ * Свалка несортированных заначек )
  */
 public class MyLib {
+    public static void main(String[] args) {
+
+        // Проверка строка или число
+        String a[]={"qw", "bbbb", "4354","34","5442","cccc"};
+
+        for (int i=0; i<=a.length-1; i++){
+            if (a[i].matches("[0-9]*")) {
+                System.out.println("a["+a[i]+"] - Число!!!");
+            } else {
+                System.out.println("a["+a[i]+"] - не является числом");
+            }
+        }
+
+        //
+    }
 }
