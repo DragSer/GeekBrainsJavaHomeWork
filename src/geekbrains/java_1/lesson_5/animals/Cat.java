6d8036bed454ba3f4734b4a1da28ef5de1f72ef2
DragSer <dragser@gmail.com>
2016-07-25 18:03:28 +0300

    HomeWork5 v1.0

--- src/geekbrains/java_1/lesson_5/animals/Cat.java
+++ src/geekbrains/java_1/lesson_5/animals/Cat.java
@@ -1,7 +1,40 @@
 package geekbrains.java_1.lesson_5.animals;
 
-/**
- * Created by xxx on 22.07.2016.
- */
-public class Cat {
+public class Cat extends Animal {
+//    protected String name;
+    protected int speed = 50;
+    protected int jump = 2;
+    protected String swim = "N";
+
+    public Cat(String name){ super(name); }
+
+    public void moveOnGround(int speed){
+        if (speed != this.speed){
+            System.out.println("1 вопрос - ошибка!");
+            super.moveOnGround();
+            System.out.print(" со скоростью "+this.speed+" км/ч");
+        } else {
+            System.out.print("1 вопрос - верно!");
+        }
+    }
+
+    @Override
+    public void swim(String swim){
+        if (swim.equals(this.swim)){
+            System.out.print("2 вопрос - верно!");
+        } else {
+            System.out.println("2 вопрос - ошибка!");
+        }
+    }
+
+    @Override
+    public void jump(int jump){
+        if (jump != this.jump){
+            System.out.println("3 вопрос - ошибка!");
+            super.jump();
+            System.out.print(" на высоту "+this.jump+" м");
+        } else {
+            System.out.print("3 вопрос - верно!");
+        }
+    }
 }
