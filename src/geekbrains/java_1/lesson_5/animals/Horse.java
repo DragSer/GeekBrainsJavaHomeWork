bf9576959cb4151bfea2ad943a97abdb5e05fa71
DragSer <dragser@gmail.com>
2016-07-25 18:03:52 +0300

    HomeWork5 v1.0

--- src/geekbrains/java_1/lesson_5/animals/Horse.java
+++ src/geekbrains/java_1/lesson_5/animals/Horse.java
@@ -1,7 +1,41 @@
 package geekbrains.java_1.lesson_5.animals;
 
-/**
- * Created by xxx on 24.07.2016.
- */
-public class Horse {
+public class Horse extends Animal {
+
+    protected int speed = 69;
+    protected int jump = 1;
+    protected String swim = "Y";
+
+    public Horse(String name) { super(name); }
+
+    @Override
+    public void moveOnGround(int speed){
+        if (speed != this.speed){
+            System.out.println("1 вопрос - ошибка!");
+            super.moveOnGround();
+            System.out.print(" со скоростью "+this.speed+" км/ч");
+        }else {
+            System.out.print("1 вопрос - верно!");
+        }
+    }
+
+    @Override
+    public void swim(String swim){
+        if (swim.equals(this.swim)){
+            System.out.print("2 вопрос - верно!");
+        } else {
+            System.out.println("2 вопрос - ошибка!");
+        }
+    }
+
+    @Override
+    public void jump(int jump){
+        if (jump != this.jump){
+            System.out.println("3 вопрос - ошибка!");
+            super.jump();
+            System.out.print(" на высоту "+this.jump+" м");
+        }else {
+            System.out.print("1 вопрос - верно!");
+        }
+    }
 }
