399fbf9df105f6b182eab6a7e2c18092bbbc4ffb
DragSer <dragser@gmail.com>
2016-08-05 15:19:42 +0300

    Отрисовка кругов

---
+++ src/geekbrains/java_1/lesson_7/MainClass.java
@@ -0,0 +1,7 @@
+package geekbrains.java_1.lesson_7;
+
+/**
+ * Created by xxx on 02.08.2016.
+ */
+public class MainClass {
+}
---
+++ src/geekbrains/java_1/lesson_7/StartNewGameWindow.java
@@ -0,0 +1,7 @@
+package geekbrains.java_1.lesson_7;
+
+/**
+ * Created by xxx on 03.08.2016.
+ */
+public class StartNewGameWindow {
+}
